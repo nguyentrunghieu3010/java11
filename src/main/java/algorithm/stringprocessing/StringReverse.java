34e1894b63e5aa4767f66a957849956dbcf5e0c0
Hieu Nguyen <hieunguyen@Hieus-MacBook-Pro.local>
2021-05-19 22:03:45 +0700

    update

--- src/main/java/algorithm/stringprocessing/StringReverse.java
+++ src/main/java/algorithm/stringprocessing/StringReverse.java
@@ -10,12 +10,9 @@ public class StringReverse {
     private static void reverseWords() {
         String input = "Nguyen Trung Hieu";
         String[] wordArr = input.split(" ");
-
         for(int i = (wordArr.length - 1); i>=0; i--) { // Reverse count
-            wordArr[i] = wordArr[i];
+            System.out.println(wordArr[i]);
         }
-//        System.out.print(Arrays.toString(wordArr));
-        Arrays.stream(wordArr).forEach(System.out::println);
     }
 
     public static void main(String[] args) {
