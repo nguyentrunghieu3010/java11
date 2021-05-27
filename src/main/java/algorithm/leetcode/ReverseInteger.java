11420c369076d7c3d7d77b092eeae543d1a9ef43
Hieu Nguyen <hieunguyen@Hieus-MacBook-Pro.local>
2021-05-27 22:13:39 +0700

    update

--- src/main/java/algorithm/leetcode/ReverseInteger.java
+++ src/main/java/algorithm/leetcode/ReverseInteger.java
@@ -12,15 +12,16 @@ public class ReverseInteger {
             x = x / 10;
         }
 
-        if(rev < Integer.MIN_VALUE || rev > Integer.MAX_VALUE) {
-            return 0;
-        } else {
-            return (int)rev;
-        }
+//        if(rev < Integer.MIN_VALUE || rev > Integer.MAX_VALUE) {
+//            return 0;
+//        } else {
+//            return (int)rev;
+//        }
+        return (int) rev == rev ? (int) rev : 0;
     }
 
     public static void main(String[] args) {
-        int input1 = 123;
+        int input1 = -123;
         System.out.println("Output1 :" + reverseInteger(input1));
     }
 }
