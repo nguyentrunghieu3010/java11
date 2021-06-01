545764c8281630e192393d0db6b3b162ed6c5880
Hieu Nguyen <hieunguyen@Hieus-MacBook-Pro.local>
2021-06-01 23:01:34 +0700

    update

--- src/main/java/algorithm/stringprocessing/PalindromeIndex.java
+++ src/main/java/algorithm/stringprocessing/PalindromeIndex.java
@@ -1,11 +1,24 @@
 package algorithm.stringprocessing;
 
+/**
+ * 9. Palindrome Number
+ */
 public class PalindromeIndex {
-    private static void palindromeIndex(String input) {
+    private static boolean palindromeIndex(int  input) {
+        if(input < 0 || (input % 10 == 0 && input != 0)) {
+            return false;
+        }
+
+        int revNum = 0;
+        while(input > revNum) {
+            revNum = revNum * 10 + input % 10;
+            input /= 10;
+        }
+        return input == revNum || input == revNum / 10;
     }
 
     public static void main(String[] args) {
-        String input = "aaab";
-        palindromeIndex(input);
+        int input = -121;
+        System.out.println(palindromeIndex(input));
     }
 }
