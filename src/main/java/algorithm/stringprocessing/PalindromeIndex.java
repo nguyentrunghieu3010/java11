33d5777dc261d463fae35fceb241ed256154e7ff
Hieu Nguyen <hieunguyen@Hieus-MacBook-Pro.local>
2021-06-02 11:45:15 +0700

    update

--- src/main/java/algorithm/stringprocessing/PalindromeIndex.java
+++ src/main/java/algorithm/stringprocessing/PalindromeIndex.java
@@ -11,14 +11,16 @@ public class PalindromeIndex {
 
         int revNum = 0;
         while(input > revNum) {
-            revNum = revNum * 10 + input % 10;
+//            revNum = revNum * 10 + input % 10;
+            revNum = revNum * 10;
+            revNum += input % 10; // get the last digit 12.1= 1
             input /= 10;
         }
         return input == revNum || input == revNum / 10;
     }
 
     public static void main(String[] args) {
-        int input = -121;
+        int input = 1212;
         System.out.println(palindromeIndex(input));
     }
 }
